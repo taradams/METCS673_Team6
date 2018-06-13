8692afb62cfe0b66369f31097029b4c3a0486f7a
Frederick Coburn <>
2018-06-13 18:29:57 -0400

    Added login/logout, authorization to view appropriate pages when signed in/out, authentication to authenticate user session.

--- src/App.js
+++ src/App.js
@@ -10,49 +10,33 @@ import PasswordForgetPage from './component/PasswordForget';
 import HomePage from './component/Home';
 import AccountPage from './component/Account';
 import * as routes from './constants/routes';
+import withAuthentication from './component/withAuthentication';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
+      
         <Router>
           <div>
             <Navigation />
-            
-            <hr/>
 
-            <Route
-              exact path={routes.LANDING}
-              component={() => <LandingPage />}
-            />
-            <Route
-              exact path={routes.SIGN_UP}
-              component={() => <SignUpPage />}
-            />
-            <Route
-              exact path={routes.SIGN_IN}
-              component={() => <SignInPage />}
-            />
-            <Route
-              exact path={routes.PASSWORD_FORGET}
-              component={() => <PasswordForgetPage />}
-            />
-            <Route
-              exact path={routes.HOME}
-              component={() => <HomePage/>}
-            />
-            <Route
-              exact path={routes.ACCOUNT}
-              component={() => <AccountPage />}
-            />
+            <hr/>
+            <Route exact path={routes.LANDING} component={() => <LandingPage />} />
+            <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
+            <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
+            <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
+            <Route exact path={routes.HOME} component={() => <HomePage />} />
+            <Route exact path={routes.ACCOUNT} component={() => <AccountPage />} />
           </div>
         </Router>
         <div className="row">
           <Columns/>
         </div>
       </div>
-    );
+   );
   }
 }
 
-export default App;
+export default withAuthentication(App);
--- src/component/Account.js
+++ src/component/Account.js
@@ -1,13 +1,21 @@
-import React,{Component} from 'react';
-  
-class AccountPage extends Component {
-  render() {
-    return (
-      <div>
-        <h1>AccountPage</h1>
-      </div>
-    );
-  }
-}
+import React from 'react';
 
-export default AccountPage;
+import AuthUserContext from './AuthUserContext';
+import { PasswordForgetForm } from './PasswordForget';
+import PasswordChangeForm from './PasswordChange';
+import withAuthorization from './withAuthorization';
+
+const AccountPage = () =>
+  <AuthUserContext.Consumer>
+    {authUser =>
+      <div>
+        <h1>Account:{authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    }
+  </AuthUserContext.Consumer>
+
+const authCondition = (authUser) => !!authUser;
+
+export default withAuthorization(authCondition)(AccountPage);
