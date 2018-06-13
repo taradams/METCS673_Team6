72f7a18d3778d27b9cd6c9b18120c4c0dcb72532
Frederick Coburn <>
2018-06-13 18:48:55 -0400

    Created a db object in firebase that syncs with our Firebase user database

--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link, withRouter, } from 'react-router-dom';
-import { auth } from '../firebase';
 
+import { auth, db } from '../firebase';
 import * as routes from '../constants/routes';
 
 const SignUpPage = ({history}) =>
@@ -42,8 +42,17 @@ class SignUpForm extends Component {
 
     auth.doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
-        this.setState(() => ({ ...INITIAL_STATE }));
-	history.push(routes.HOME);
+        
+        // Create a user in your own accessible Firebase Database too
+        db.doCreateUser(authUser.user.uid, username, email)
+          .then(() => {
+            this.setState(() => ({ ...INITIAL_STATE }));
+            history.push(routes.HOME);
+          })
+          .catch(error => {
+            this.setState(byPropKey('error', error));
+          });      
+
       })
       .catch(error => {
         this.setState(byPropKey('error', error));
