9e5ab91495b1e4a366ead327d19b16834d537c70
Frederick Coburn <>
2018-06-14 14:19:27 -0400

    Updated to use redux for state control

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './app';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+if (module.hot) {
+  module.hot.accept();
+}
+
 registerServiceWorker();
