7d3c1abba0dfa65074b796a110c0f1ae10fa72b2
xijun <yunxi.alexis.yang@gmail.com>
2018-06-19 19:30:08 -0400

    Add a column

--- src/container/Columns.jsx
+++ src/container/Columns.jsx
@@ -1,23 +1,48 @@
 import React from 'react';
 import Column from '../component/Column.jsx';
 import './Columns.css';
+import TextareaAutosize from 'react-autosize-textarea';
 
 export default class Columns extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            columns: [{
+                title: "Backlog"   
+            }, {
+                title: "In development"
+            }, {
+                title: "Done"
+            }],
+            addListValue: ""
+        }
+        this.handleInputAddColumnField = this.handleInputAddColumnField.bind(this);
+        this.handleAddListClick = this.handleAddListClick.bind(this);
+    }
+
+    handleInputAddColumnField(e) {
+        this.setState({addListValue: e.target.value});
+    }
+
+    handleAddListClick() {
+        this.state.columns.push({ title: this.state.addListValue });
+        this.setState({ addListValue: "", columns: this.state.columns });
     }
 
     render() {
         return (
         <div id="Columns">
+            {
+                this.state.columns.map((column) => {
+                    return  (<div className="container">
+                        <Column title={column.title} />
+                    </div>)
+                })
+            }
             <div className="container">
-                    <Column title="Backlog" />
-            </div>
-            <div className="container">
-                    <Column title="In development"/>
-            </div>
-            <div className="container">
-                    <Column title="Done" />
+                <TextareaAutosize value={this.state.addListValue} onChange={this.handleInputAddColumnField} placeholder="Add a list" />
+                <br/>
+                <button id="Add" onClick={this.handleAddListClick} className="btn">Add</button>
             </div>
         </div>
         );
