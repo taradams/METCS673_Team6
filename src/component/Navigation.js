c275c64d38bb6a299f6b7bc04ed8dc9246f9ee09
Frederick Coburn <>
2018-06-19 19:02:45 -0400

    Simplified navigator

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -15,16 +15,12 @@ const Navigation = ({authUser}) =>
 
 const NavigationAuth = () =>
   <ul>
-    <li><Link to={routes.LANDING}>Landing</Link></li>
-    <li><Link to={routes.HOME}>Home</Link></li>
-    <li><Link to={routes.ACCOUNT}>Account</Link></li>
     <li><Link to={routes.PROJECT_MANAGER}>Project Manager</Link></li>
     <li><SignOutButton /></li>
   </ul>
 
 const NavigationNonAuth = () =>
   <ul>
-    <li><Link to={routes.LANDING}>Landing</Link></li>
     <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
   </ul>
 
