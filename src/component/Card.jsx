d38a513bebf5651730323d59c7c7ab6a35bfdd4a
xijun <yunxi.alexis.yang@gmail.com>
2018-06-28 20:59:54 -0400

    card hidden while dragging

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -44,7 +44,6 @@ class Card extends React.Component {
             text: "edit this text!"
         };
 
-        //experimental code below
         this._handleFocus = this._handleFocus.bind(this);
         this._handleFocusOut = this._handleFocusOut.bind(this);
         this.onClickDelete = this.onClickDelete.bind(this);        
@@ -67,33 +66,36 @@ class Card extends React.Component {
     //props 
     render() {
         const { isDragging, connectDragSource } = this.props
-
-        return (connectDragSource && connectDragSource(
-            <div className="card task box">
-                <div className="card-body">
-                    {
-                        this.props.card.content
-                    }
-                    <button type="button" style={{float: "right"}} onClick={this.onClickDelete}  className="btn">X</button>
-                </div>
-                {/* <div>
-                    <EditableLabel 
-                        text= {this.state.text}
-                        labelClassName='myLabelClass'
-                        inputClassName='myInputClass'
-                        inputWidth='200px'
-                        inputHeight='25px'
-                        //inputMaxLength='50'
-                        labelFontWeight='bold'
-                        inputFontWeight='bold'
-                        onFocus={this._handleFocus}
-                        onFocusOut={this._handleFocusOut}
-                    />   
-                    {
-                        this.props.card.content = this.state.text
-                    }                     
-                </div> */}
+        const toRender = !isDragging ? 
+            (<div className="card task box">
+            <div className="card-body">
+                {
+                    this.props.card.content
+                }
+                <button type="button" style={{float: "right"}} onClick={this.onClickDelete}  className="btn">X</button>
             </div>
+            {/* <div>
+                <EditableLabel 
+                    text= {this.state.text}
+                    labelClassName='myLabelClass'
+                    inputClassName='myInputClass'
+                    inputWidth='200px'
+                    inputHeight='25px'
+                    //inputMaxLength='50'
+                    labelFontWeight='bold'
+                    inputFontWeight='bold'
+                    onFocus={this._handleFocus}
+                    onFocusOut={this._handleFocusOut}
+                />   
+                {
+                    this.props.card.content = this.state.text
+                }                     
+            </div> */}
+            </div>)
+            :
+            <div/>
+        return (connectDragSource && connectDragSource(
+            toRender
         ));
     }
 }
