dc99e2e45783e6d88d81d677ee5a034f3ae030ba
tarawinn <tarawinnadams@gmail.com>
2018-06-29 20:26:20 -0400

    assigned tasks between Tara and Fred

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,15 +10,40 @@ var task_controller = require('../controllers/taskCtrk');
 
 
 
-/* ACCOUNT ROUTES */
+/* ACCOUNT ROUTES */ //TARA
 
-/* COLUMN ROUTES */
+//create account
+//login
+//logout
+
+/* COLUMN ROUTES */ //TARA
+
+//create column
+//update column
+//display tasks in column
+//delete columns
 
 /* COMMENT ROUTES */
 
-/* TASK ROUTES */
+/* TASK ROUTES */ //FRED
+
+//display tasks
+//create task
+//delete task
+//update task column
+//update task information
 
 /* TASK (type = bug) ROUTES */
+//display issues
+//create issue
+//delete issue
+//update issue column
+//update issue information
+
+/* CHAT ROUTES */   //TARA
+//create group chat
+//display messages
+//create message
 
 
 
