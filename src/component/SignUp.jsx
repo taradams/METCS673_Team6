d7e42a0e50dec214b09b9d922d618a373bfa1903
xijun <yunxi.alexis.yang@gmail.com>
2018-07-12 20:18:40 -0400

    clean code

---
+++ src/component/SignUp.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { auth, db } from '../firebase';
+import * as routes from '../constants/routes';
+import { INITIAL_STATE } from '../reducers/SignUp';
+
+const byPropKey = (propertyName, value) => () => ({
+  [propertyName]: value,
+});
+
+class SignUpForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = (event) => {
+    const {
+      username,
+      email,
+      passwordOne,
+    } = this.state;
+    
+    const {
+      history,
+    } = this.props
+
+    auth.doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(authUser => {
+        
+        // Create a user in your own accessible Firebase Database too
+        db.doCreateUser(authUser.user.uid, username, email)
+          .then(() => {
+            this.setState(() => ({ ...INITIAL_STATE }));
+            history.push(routes.HOME);
+          })
+          .catch(error => {
+            this.setState(byPropKey('error', error));
+          });      
+
+      })
+      .catch(error => {
+        this.setState(byPropKey('error', error));
+      });
+
+    event.preventDefault();
+  }
+
+  render() {
+    const {
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      error,
+    } = this.state;
+
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === '' ||
+      email === '' ||
+      username === '';
+   
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={username}
+          onChange={event => this.setState(byPropKey('username', event.target.value))}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          value={email}
+          onChange={event => this.setState(byPropKey('email', event.target.value))}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          value={passwordOne}
+          onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          value={passwordTwo}
+          onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign Up
+        </button>
+
+        { error && <p>{error.message}</p> }
+      </form>
+    );
+  }
+}
+
+export default SignUpForm;
---
+++ src/container/Navigation.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import SignOutButton from '../component/SignOut';
+import * as routes from '../constants/routes';
+
+class Navigation extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authUser: props.authUser
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+        this.setState({
+            authUser: nextProps.authUser
+        });
+  }
+  render() {
+    const NavigationAuth = () => (
+      <ul>
+        <li><Link to={routes.PROJECT_MANAGER}>Project Manager</Link></li>
+        <li><Link to={routes.ISSUE_TRACKER}>Issue Tracker</Link></li>
+        <li><Link to={routes.CHAT}>Chat</Link></li>
+        <li><SignOutButton /></li>
+      </ul>);
+
+    const NavigationNonAuth = () => (
+      <ul>
+        <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
+      </ul>);
+
+    return (
+    <div>
+      { this.state.authUser
+          ? <NavigationAuth />
+          : <NavigationNonAuth />
+      }
+    </div>)
+  }
+}
+
+const mapStateToProps = (state) => ({
+  authUser: state.sessionState.authUser,
+});
+
+export default connect(mapStateToProps)(Navigation);
