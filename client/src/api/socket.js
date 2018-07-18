199cb3ea8b4af0dddacd6c75e268044737184039
xijun <yunxi.alexis.yang@gmail.com>
2018-07-18 01:36:32 -0400

    fix socket

--- client/src/api/socket.js
+++ client/src/api/socket.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client/dist/socket.io';
-const  socket = openSocket('https://metcs673-team6.herokuapp.com/');
+const  socket = openSocket('/');
 
 function receiveUpdate(cb) {
     socket.on('receiveUpdate', () => cb());
