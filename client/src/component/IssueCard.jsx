d06f671026b8e8478479fd38e3c4729a9ee85462
tarawinn <tarawinnadams@gmail.com>
2018-07-19 22:14:56 -0400

    merging UI changes and edit task/issue functionality

--- client/src/component/IssueCard.jsx
+++ client/src/component/IssueCard.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
-import './IssueCard.css'
+import './IssueCard.css';
+import IssueCardModal from './IssueCardModal';
 
 class IssueCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+    this.state = {
+        showModal: false,//new stuff for modal
+
+    };
+
+    //new modal method
+    this.handleToggleModal =this.handleToggleModal.bind(this);
+
+    }
+
+    handleToggleModal() {
+        this.setState({ showModal: !this.state.showModal });
+    }
+
 
     render() {
+        const { showModal } = this.state;
+
         return(
         <div className="IssueCard">
             <h5>{this.props.issue.title}</h5>
             <div className="horizontal-line" />
             <p>{this.props.issue.details}</p>
+            <button
+                type="button"
+                className="issueModalButton"
+                onClick={() => this.handleToggleModal()}
+                >
+                {/* Modal */}
+                <i class="fas fa-edit"></i>
+                </button>
+                {showModal &&
+                    <IssueCardModal 
+                    onCloseRequest={() => this.handleToggleModal()} 
+                    cardTitle={this.props.issue.title}
+                    cardDescription={this.props.issue.details}
+                    cardID={this.props.issue.id}/>}
         </div>
     )
     }
--- client/src/component/IssueForm.jsx
+++ client/src/component/IssueForm.jsx
@@ -41,18 +41,18 @@ class IssueForm extends Component{
                 <div>
                     <textarea 
                     className="issue-title" 
-                    placeholder="Issue Title" 
+                    placeholder="New Issue Title" 
                     value={this.state.titleInput} 
                     onChange={this.handleTitleChange}/>
                 </div>
                 <div>
                     <textarea 
                     className="issue-description" 
-                    placeholder="Write a description..." 
+                    placeholder="Write a new description..." 
                     value={this.state.descriptionInput} 
                     onChange={this.handleDescriptionChange}/>
                 </div>
-                <button type="button" onClick={this.createNewIssueBtn}>Create New Issue</button>
+                <button className="submitNewIssueBtn" type="button" onClick={this.createNewIssueBtn}>Create New Issue</button>
             </form>
         );
     }
