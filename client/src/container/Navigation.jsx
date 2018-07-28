0a1c1e7dea87a8a0c8902f0a6c03d6e8df02845c
rdyason <robdyason@gmail.com>
2018-07-28 17:17:40 -0400

    Add files via upload

--- client/src/container/Navigation.jsx
+++ client/src/container/Navigation.jsx
@@ -1,53 +1,66 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import './Navigation.css'
 
 import SignOutButton from '../component/SignOut';
 import * as routes from '../constants/routes';
 
+import './Navigation.css';
 
 class Navigation extends React.Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      authUser: props.authUser
-    };
-  }
+        this.state = {
+            authUser: props.authUser,
+            bool_toggle: false,
+            my_route: routes.CHAT
+        };
+        this.toggle_chat.bind(this);
+    }
 
-  componentWillReceiveProps(nextProps) {
+    componentWillReceiveProps(nextProps) {
         this.setState({
             authUser: nextProps.authUser
         });
-  }
-  render() {
-    const NavigationAuth = () => (
-      <ul>
-        <li><Link to={routes.PROJECT_MANAGER}>Project Manager</Link></li>
-        <li><Link to={routes.ISSUE_TRACKER}>Issue Tracker</Link></li>
-        <li><Link to={routes.CHAT}>Chat</Link></li>
-        <li><SignOutButton /></li>
-      </ul>);
+    }
 
-    const NavigationNonAuth = () => (
-      <ul>
-        <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
-      </ul>);
+    toggle_chat(toggle) {
+        if (toggle) {
+            this.setState({ my_route: routes.CHAT, bool_toggle: !this.state.bool_toggle });
+        } else if (!toggle) {
+            this.setState({ my_route: routes.PROJECT_MANAGER, bool_toggle: !this.state.bool_toggle });
+        }
+    }
 
-    return (
-    <div>
-      { this.state.authUser
-          ? <NavigationAuth />
-          : <NavigationNonAuth />
-      }
-    </div>)
-  }
+    render() { 
+        const NavigationAuth = () => (
+            <ul className="nav_bar">
+                <li><Link className="nav_button" to={routes.PROJECT_MANAGER}>Project Manager</Link></li>
+                <li><Link className="nav_button" to={routes.ISSUE_TRACKER}>Issue Tracker</Link></li>
+                <li><Link className="nav_button" to={this.state.my_route} onClick={() => { this.toggle_chat(this.state.bool_toggle) }}>Chat</Link></li>
+                <li className="sign_out_button"><SignOutButton /></li>
+            </ul>
+        );
+
+        const NavigationNonAuth = () => (
+            <ul className="nav_bar">
+                <li><Link className="nav_button" to={routes.SIGN_IN}>Sign In</Link></li>
+            </ul>);
+
+        return (
+            <div>
+                {this.state.authUser
+                    ? <NavigationAuth />
+                    : <NavigationNonAuth />
+                }
+            </div>)
+    }
 }
 
 const mapStateToProps = (state) => ({
-  authUser: state.sessionState.authUser,
+    authUser: state.sessionState.authUser,
 });
 
 export default connect(mapStateToProps)(Navigation);
--- client/src/container/Page.jsx
+++ client/src/container/Page.jsx
@@ -14,30 +14,60 @@ import IssueTrackerPage from '../component/IssueTracker';
 import ChatPage from '../component/chat/Chat';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContextProvider } from 'react-dnd';
+import Room from '../component/chat/Room.js';
+
+import './Page.css';
 
 class Page extends React.Component {
 
-  render() {
-    return (
-      <div>
-      <Router>
-        <div>
-          <Navigation />
-          <hr/>
-            <Route exact path={routes.LANDING} component={() => <LandingPage />} />
-            <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
-            <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
-            <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
-            <Route exact path={routes.PROJECT_MANAGER} component={() =>
-              <DragDropContextProvider backend={HTML5Backend}>
-                <ProjectManagerPage />
-              </DragDropContextProvider>} />
-            <Route exact path={routes.ISSUE_TRACKER} component={() => <IssueTrackerPage />} />
-            <Route exact path={routes.CHAT} component={() => <ChatPage />} />
-        </div>
-      </Router>
-    </div>);
-  }
+    constructor(props) {
+        super(props);
+        this.state = { show: true }
+    }
+
+    render() {
+        return (
+            <div>
+                <Router>
+                    <div>
+                        <Navigation />
+                        <div className="canvas">
+                            <DragDropContextProvider backend={HTML5Backend}>
+                            <Route exact path={routes.LANDING} component={() => <LandingPage />} />
+                            <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
+                            <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
+                            <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
+                            <Route exact path={routes.PROJECT_MANAGER} component=
+                                {() =>
+                                    <div className="pmt_lonely">
+                                        {/*<DragDropContextProvider backend={HTML5Backend}>*/}
+                                            <ProjectManagerPage />
+                                        {/*</DragDropContextProvider>*/}
+                                    </div>
+                                }
+                            />
+                            <Route exact path={routes.ISSUE_TRACKER} component={() => <IssueTrackerPage />} />
+                            <Route exact path={routes.CHAT} component=
+                                {() =>
+                                    <div className="flex_box">
+                                        <div className="pmt_component">
+                                            {/*<DragDropContextProvider backend={HTML5Backend}>*/}
+                                                <ProjectManagerPage style="margin: 0; padding: 0;"/>
+                                            {/*</DragDropContextProvider>*/}
+                                        </div>
+                                        <div className="chat_component" >
+                                            <Room />
+                                        </div>
+                                    </div>
+                                }
+                                />
+                            </DragDropContextProvider>
+                        </div>
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default withAuthentication(Page);
