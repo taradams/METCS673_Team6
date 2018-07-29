77153d331b76b67c43b9ec1bad11f73fdb2b0dec
tarawinn <tarawinnadams@gmail.com>
2018-07-29 19:11:38 -0400

    added redux thunk to package.json

--- controllers/accountCtrl.js
+++ controllers/accountCtrl.js
@@ -123,6 +123,8 @@ exports.get_user = function(req,res,next){
             }else{
                 res.json({ message: 'no user session' });
                 console.log('no session');
+                err.status = 401;
+                return next(err);
                 //return something else here?
             }
         };
